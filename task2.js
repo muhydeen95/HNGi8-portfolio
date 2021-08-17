5dbc741d629ebf09db7331e23fd3db4aec667f6a
ALABI MUHYDEEN OLANIYI <48595796+muhydeen95@users.noreply.github.com>
2021-08-17 16:51:43 +0100

    assets and style folder pushed

---
+++ task2.js
@@ -0,0 +1,6 @@
+let firstName = 'Muhydeen';
+let middlename = 'Olaniyi';
+let lastName = 'Alabi';
+
+let myName = document.getElementById('name');
+myName.innerText = `${lastName + ' ' + firstName + ' ' + lastName}`
